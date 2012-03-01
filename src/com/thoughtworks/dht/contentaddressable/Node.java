48672cee53f3593f1670a44209fc7956ab8a0c37
Karan Misra <karan@erodov.com>
2012-03-01 13:28:52 +0530

    Added split operation on Node

--- src/com/thoughtworks/dht/contentaddressable/Node.java
+++ src/com/thoughtworks/dht/contentaddressable/Node.java
@@ -9,19 +9,35 @@ public class Node {
         this.bottomLeft = bottomLeft;
         this.topRight = topRight;
     }
-    
+
     private boolean sharesHorizontalEdgeWith(Node other) {
         return topRight.verticallyAlignedTo(other.bottomLeft) &&
-               ((topRight.aheadOfHorizontally(other.bottomLeft) && other.topRight.aheadOfHorizontally(bottomLeft)));
+                topRight.aheadOfHorizontally(other.bottomLeft) &&
+                other.topRight.aheadOfHorizontally(bottomLeft);
     }
-    
+
     private boolean sharesVerticalEdgeWith(Node other) {
         return topRight.horizontallyAlignedTo(other.bottomLeft) &&
-               ((topRight.aheadOfVertically(other.bottomLeft) && other.topRight.aheadOfVertically(bottomLeft)));
+                topRight.aheadOfVertically(other.bottomLeft) &&
+                other.topRight.aheadOfVertically(bottomLeft);
     }
 
     public boolean isAdjacentTo(Node other) {
-        return this.sharesHorizontalEdgeWith(other) || other.sharesHorizontalEdgeWith(this) ||
-               this.sharesVerticalEdgeWith(other) || other.sharesVerticalEdgeWith(this);
+        return sharesHorizontalEdgeWith(other) || other.sharesHorizontalEdgeWith(this) ||
+                sharesVerticalEdgeWith(other) || other.sharesVerticalEdgeWith(this);
+    }
+
+    public Node[] splitHorizontal() {
+        Node node1 = new Node(bottomLeft, new Point(topRight.x(), (topRight.y() + bottomLeft.y()) / 2));
+        Node node2 = new Node(new Point(bottomLeft.x(), (topRight.y() + bottomLeft.y()) / 2), topRight);
+
+        return new Node[]{node1, node2};
+    }
+
+    public Node[] splitVertical() {
+        Node node1 = new Node(bottomLeft, new Point((topRight.x() + bottomLeft.x()) / 2, topRight.y()));
+        Node node2 = new Node(new Point((topRight.x() + bottomLeft.x()) / 2, bottomLeft.y()), topRight);
+
+        return new Node[]{node1, node2};
     }
 }
--- test/com/thoughtworks/dht/contentaddressable/NodeTest.java
+++ test/com/thoughtworks/dht/contentaddressable/NodeTest.java
@@ -2,6 +2,7 @@ package com.thoughtworks.dht.contentaddressable;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -35,5 +36,48 @@ public class NodeTest {
     @Test
     public void nonStackedNodesAreNotNeighbours() {
         assertFalse(new Node(new Point(0.0, 0.0), new Point(0.25, 0.25)).isAdjacentTo(new Node(new Point(0.5, 0.5), new Point(1, 1))));
+    }
+    
+    @Test
+    public void canSplitItselfHorizontallyAndStillBeNeighbours() {
+        Node parentNode = from(0.0, 0.0, 1.0, 1.0);
+        
+        Node[] split = parentNode.splitHorizontal();
+        
+        assertEquals(2, split.length);
+        assertTrue(split[0].isAdjacentTo(split[1]));
+        assertTrue(split[1].isAdjacentTo(split[0]));
+    }
+
+    @Test
+    public void canSplitItselfVerticallyAndStillBeNeighbours() {
+        Node parentNode = from(0.0, 0.0, 1.0, 1.0);
+
+        Node[] split = parentNode.splitVertical();
+
+        assertEquals(2, split.length);
+        assertTrue(split[0].isAdjacentTo(split[1]));
+        assertTrue(split[1].isAdjacentTo(split[0]));
+    }
+    
+    @Test
+    public void canSplitItselfThreeTimesAndAllOfThemWillStillBeNeighbours() {
+        Node parentNode = from(0.0, 0.0, 1.0, 1.0);
+        
+        Node[] split1 = parentNode.splitHorizontal();
+        Node[] split2 = split1[0].splitVertical();
+
+        assertNeighboursWith(split1[1], split2);
+        assertNeighboursWith(split2[0], split1[1], split2[1]);
+        assertNeighboursWith(split2[1], split1[1], split2[0]);
+    }
+    
+    private void assertNeighboursWith(Node parentNode, Node... nodes) {
+        for(Node node : nodes)
+            assertTrue(parentNode.isAdjacentTo(node));
+    }
+
+    private Node from(double blX, double blY, double trX, double trY) {
+        return new Node(new Point(blX, blY), new Point(trX,  trY));
     }
 }
